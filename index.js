f76d6ec7cc8d33f91ab99f8d78334e2891c9bb4a
Nightfury0207 <104409153+Nightfury0207@users.noreply.github.com>
2023-05-07 18:57:59 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,8 @@ const body_parser=require("body-parser");
 const axios=require("axios");
 const mysql=require("mysql");
 const con=mysql.createConnection({
-    host:"122.180.254.41,3366",
+    host:"122.180.254.41",
+    port: 1433,
     user:"info_crmtest",
     password:"Hawamahal@123",
     database:"info_whatsapp"
