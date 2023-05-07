ccc74e00db0ce08a7e4247572c0411796ca76ddf
Nightfury0207 <104409153+Nightfury0207@users.noreply.github.com>
2023-05-08 00:28:23 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const axios=require("axios");
 const mysql=require("mysql");
 const con=mysql.createConnection({
     host:"tswserver.theworkpc.com",
-    username:"amitktsw",
+    user:"amitktsw",
     password:"Hawamahal@12#",
     database:"whatsapp"
 });
