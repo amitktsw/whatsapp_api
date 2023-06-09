634d15782da90aa5fad4b19fbdb7fd42dcbf396b
Nightfury0207 <104409153+Nightfury0207@users.noreply.github.com>
2023-06-09 17:54:12 +0530

    Update index.js

--- index.js
+++ index.js
@@ -69,7 +69,7 @@ app.post("/webhook",(req,res)=>{ //i want some
                console.log("from: "+from);
                console.log("boady param: "+msg_body);
             
-               var sql = "INSERT INTO received_messages (message_to,message_from,message) VALUES ?";
+               var sql = "INSERT INTO received_messages (msg_to,msg_from,msg) VALUES ?";
                var values=[phon_no_id,from,msg_body];
                con.query(sql,[[[phon_no_id,from,msg_body]]], function (err, result) {  
                 if (err) throw err;  
